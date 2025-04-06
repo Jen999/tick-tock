d2dd32da67a7ebd4a5a0fc9231c09838e08b886e
Andy Chong <andychong23@gmail.com>
2025-04-06 18:39:04 +0800

    Implement Unit Testing for SessionService
    Implementation:
    1. Create an expected json file in test/resources
    2. Implement createAndSaveSession test
    
    If test fails, the file created will not be deleted in data/
    Thus, debugging of the test cases can occur

--- src/test/java/com/ticktock/service/SessionServiceTest.java
+++ src/test/java/com/ticktock/service/SessionServiceTest.java
@@ -2,12 +2,41 @@ package com.ticktock.service;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class SessionServiceTest {
-    private static final String FILE_NAME = "/sessionService.json";
+    private static final String RESOURCE_FILE_NAME = "/sessionServiceExpected.json";
     private static final String RESOURCE_FOLDER_NAME = "/service/SessionService";
+    private static final String SAVE_FILE_NAME = "actualSessionService.json";
 
     @Test
-    public void createAndSaveSession() {
-        return;
+    public void createAndSaveSession() throws IllegalAccessException {
+        SessionService.createAndSaveSession(
+                "cs2100",
+                "proj",
+                100,
+                "00:20:30",
+                "00:10:00",
+                List.of("00:02:30", "00:05:25", "00:02:05"),
+                SAVE_FILE_NAME
+        );
+
+        List<SessionRecord> expectedSessionRecords = ServiceUtil.loadSessionsFromTestResource(RESOURCE_FOLDER_NAME, RESOURCE_FILE_NAME);
+        List<SessionRecord> actualSessionRecords = StorageService.loadSessions(SAVE_FILE_NAME);
+
+        // ensures same number of records
+        assertEquals(expectedSessionRecords.size(), actualSessionRecords.size());
+
+        for (int i = 0; i < expectedSessionRecords.size(); i++) {
+            assertTrue(ServiceUtil.isSessionRecordEqual(expectedSessionRecords.get(i), actualSessionRecords.get(i)));
+        }
+
+        // Ensures file saved is deleted
+        Paths.get("data" + File.separator + SAVE_FILE_NAME).toFile().deleteOnExit();
     }
 }
