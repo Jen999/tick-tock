5aaa9c2dac203e0c10818b8ef2b858320971182b
Andy Chong <79183296+andychong23@users.noreply.github.com>
2025-04-18 17:24:57 +0800

    Remove unused imports and replace wildcard imports

--- src/main/java/com/ticktock/service/StorageService.java
+++ src/main/java/com/ticktock/service/StorageService.java
@@ -2,16 +2,20 @@ package com.ticktock.service;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
 import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-
-import com.google.gson.reflect.TypeToken;
 
 /**
  * Handles JSON reading and writing
